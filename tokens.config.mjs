bb7bf0091526ac4f366c091d43bf22b311307d2f
tinykite <dakota@tinykitelab.com>
2024-06-07 13:22:16 -0500

    Add cobalt configuration and auto-built custom variables

---
+++ tokens.config.mjs
@@ -0,0 +1,8 @@
+import pluginCSS from "@cobalt-ui/plugin-css";
+
+/** @type {import("@cobalt-ui/core").Config} */
+export default {
+  tokens: "./tokens.json",
+  outDir: "./tokens/",
+  plugins: [pluginCSS()],
+};
